c052ada99066034b997d1e746a470f2d72a8071c
Md. Atikur  Rahman <contact.atikurrahman@gmail.com>
2021-09-07 17:29:57 +0600

    added

--- binarysearch.js
+++ binarysearch.js
@@ -1,58 +1,28 @@
-function binarySearch(arr,find){
-
-
-
+function binarySearch(arr, find) {
     var len = arr.length;
-
     var left = 0;
     var right = len - 1;
-    var mid = Math.floor((right+left)/2);
-         
- 
-    console.log('lenght ' +len);
-    console.log('mid ' +mid);
-    console.log('left ' +left);
-    console.log('right ' +right);
-
-
-
-    while(left <= right){
-
-
+    while (left <= right) {
+        var mid = Math.floor((right + left) / 2);
         if (arr[mid] == find) {
-
-            console.log(find+ 'found in the position' +mid);
+           
+            console.log(find + ' found in the position ' + mid);
+            return find;
 
         } else if (arr[mid] < find) {
 
-
+            left = mid + 1;
         } else {
 
             right = mid - 1;
-
-
-
         }
 
-
-
     }
-    
-  
-
-   console.log('out of the array');
-
-
-
-
-
-
-
+    console.log('out of the array');
 }
 
+var arr = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'];
+var find = 18;
 
-var arr = ['1','2','3','4','5','6','7','8','9','10'];
-var find = 7;
-
-binarySearch(arr,find)
+binarySearch(arr, find)
 
